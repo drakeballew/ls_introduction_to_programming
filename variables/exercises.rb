e91f542c0af56cf9d09882e94be41e6dc6030632
Drake Ballew <drakeballew@hotmail.com>
2017-07-10 16:40:25 -0700

    Add variables exercises

---
+++ variables/exercises.rb
@@ -0,0 +1,35 @@
+# 1 (name.rb)
+# puts 'What\'s your name?'
+# name = gets.chomp
+# puts "Hi, #{name}!"
+
+# 2 (age.rb)
+# puts 'How old are you?'
+# age   = gets.chomp.to_i
+# years = 0
+
+# 4.times do
+#   years += 10
+#   puts "In #{years} years, you will be #{age+years}."
+# end
+
+# 3 (name.rb extended)
+# puts 'What\'s your name?'
+# name = gets.chomp
+# 10.times do 
+#   puts name
+# end
+
+# 4 (name.rb extended_again)
+# puts 'What\'s your first name?'
+# first = gets.chomp
+# puts 'What\'s your last name?'
+# last = gets.chomp
+# puts first + ' ' + last
+
+# 5
+# The first prints 3 to the screen.
+# The second throws an undefined local variable error because x is not available outside the scope of the do/end block. 
+
+# 6
+# It tells you that the variable or method 'shoes' is being called outside of its scope or has not been defined in the program.
